a018e75c83e1e6df51881cb8c0d168cc1dbcbcef
Lukinhasssss <l.andradesilva1006@gmail.com>
2026-07-28 11:09:01 -0300

    Lição 6

---
+++ licao6.php
@@ -0,0 +1,16 @@
+<?php
+
+$numero1 = 10;
+
+$numero2 = 5;   
+
+if($numero1 > $numero2)
+    {
+        echo "O número 1 é maior que o número 2";
+    }
+else
+    {
+        echo "O número 2 é maior que o número 1";
+    }
+
+?>
