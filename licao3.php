b74879031bcb23d6fe2bf2db48fa35bee9d180b8
Lukinhasssss <l.andradesilva1006@gmail.com>
2026-07-28 10:38:04 -0300

    Lição 3

---
+++ licao3.php
@@ -0,0 +1,22 @@
+<?php
+
+$numero1 = 10;
+$numero2 = 10;
+
+$soma = $numero1 + $numero2;
+
+$subtracao = $numero1 - $numero2;
+
+$divisao = $numero1 / $numero2;
+
+$multiplicacao = $numero1 * $numero2;
+
+echo "O resultado da soma é: $soma";
+echo "<br>";
+echo "O resultado da subtração é: $subtracao";
+echo "<br>";
+echo "O resultado da divisão é: $divisao";
+echo "<br>";
+echo "O resultado da multiplicação é: $multiplicacao";
+
+?>
