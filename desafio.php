bad7998192e8a59e2d13076e6f7b8fd6b95b782f
Lukinhasssss <l.andradesilva1006@gmail.com>
2026-07-28 11:35:05 -0300

    Desafio

---
+++ desafio.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "Lucas";
+$idade = 18;
+
+if($idade >= 18)
+    {
+        echo "Cliente: $nome";
+        echo "<br>";
+        echo "Idade: $idade anos";
+        echo "<br>";
+        echo "Empréstimo liberado";
+    }
+else
+    {
+        echo "Cliente: $nome";
+        echo "<br>";
+        echo "Idade: $idade anos";
+        echo "<br>";
+        echo "Empréstimo negado";
+    }
+
+?>
