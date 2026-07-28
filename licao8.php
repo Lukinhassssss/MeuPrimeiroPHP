a1a94ef6fc3ed7d81af4b89e2a085e5e37a76de9
Lukinhasssss <l.andradesilva1006@gmail.com>
2026-07-28 11:21:16 -0300

    Lição 8

---
+++ licao8.php
@@ -0,0 +1,16 @@
+<?php
+
+$usuario = "admin";
+
+$senha = "1234";
+
+if ($usuario == "admin" && $senha == "1234")
+    {
+        echo "Login realizado com sucesso";
+    }
+else
+    {
+        echo "Usuário ou senha inválidos";
+    }
+
+?>
